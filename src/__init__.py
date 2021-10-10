5f09efa0078de404142a05216061c16565b36ceb
joeylam <joey.cm.lam@gmail.com>
2021-10-10 22:08:53 +0800

    make the connection to IB

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,10 @@ import logging
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='../log/ibportfolio.log', level=logging.INFO)
+    FORMAT = '%(asctime)-15s %(message)s'
+    logging.basicConfig(filename='../log/ibportfolio.log', format=FORMAT, level=logging.DEBUG)
+
+    logging.debug('debug test')
     logging.info('start!')
 
     logging.info('completed!')
