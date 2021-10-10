15b90c1137bdba4458315363b115a90453886433
joeylam <joey.cm.lam@gmail.com>
2021-10-10 21:58:08 +0800

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='../log/ibportfolio.log', level=logging.INFO)
+    logging.info('start!')
+
+    logging.info('completed!')
