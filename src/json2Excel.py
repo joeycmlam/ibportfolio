596bd109652168e2fefc01515dcc32bf4d82e9bf
joeylam <joey.cm.lam@gmail.com>
2021-10-23 17:45:34 +0800

    convert nested json to excel

---
+++ src/json2Excel.py
@@ -0,0 +1,33 @@
+import logging
+import json
+import pandas as pd
+
+jsonFile = "../data/sample-nest.json"
+outFilename = "../output/data-nest.xlsx"
+
+def read_json(filename: str) -> dict:
+    try:
+        with open(filename, "r") as f:
+            data = json.loads(f.read())
+    except:
+        raise Exception(f"Reading {filename} file encountered an error")
+
+    return data
+
+def main():
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.DEBUG)
+    try:
+        data = read_json(filename="../data/sample-nest.json")
+        df = pd.json_normalize(data, max_level=2)
+        df.to_excel(outFilename)
+    except Exception as ex:
+        logging.error(ex)
+    finally:
+        logging.info('Done')
+
+
+if __name__ == '__main__':
+    main()
+
