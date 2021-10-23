f6cec98302885a2cb8914b9dace3195aec930adb
joeylam <joey.cm.lam@gmail.com>
2021-10-23 22:33:00 +0800

    convert nested list of json to excel

--- src/json2Excel.py
+++ src/json2Excel.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 jsonFile = "../data/sample-nest.json"
-outFilename = "../output/data-nest.xlsx"
+
 
 def read_json(filename: str) -> dict:
     try:
@@ -14,29 +14,34 @@ def read_json(filename: str) -> dict:
 
     return data
 
+
 def main():
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                         datefmt='%Y-%m-%d:%H:%M:%S',
                         level=logging.DEBUG)
     try:
         data = read_json(filename="../data/sample-nest.json")
+        outFilename = "../output/data-nest.xlsx"
         df = pd.json_normalize(data, max_level=2)
         df.to_excel(outFilename)
     except Exception as ex:
         logging.error(ex)
     finally:
         logging.info('Done')
+
 
 def main_list():
     try:
         data = read_json(filename="../data/sample-nest-list.json")
-        df = pd.json_normalize(data, max_level=2)
+        outFilename = "../output/data-nest-list.xlsx"
+        df = pd.json_normalize(data, record_path=['funds'],
+                               meta=['id', 'firstName', 'lastName', 'email', 'country'])
         df.to_excel(outFilename)
     except Exception as ex:
         logging.error(ex)
     finally:
         logging.info('Done')
 
+
 if __name__ == '__main__':
     main_list()
-
