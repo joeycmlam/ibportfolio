d6063f6b0779d78b2cb7eee1a6d630904c316676
joeylam <joey.cm.lam@gmail.com>
2021-10-30 18:18:38 +0800

    as to_excel is going decommission, build the function to replace it

--- src/json2Excel.py
+++ src/json2Excel.py
@@ -1,8 +1,10 @@
 import logging
 import json
 import pandas as pd
+import xlwt
 
 jsonFile = "../data/sample-nest.json"
+
 
 
 def read_json(filename: str) -> dict:
@@ -14,26 +16,40 @@ def read_json(filename: str) -> dict:
 
     return data
 
+def write2excel(fileName, sheetName, data):
+    logging.info('write2excel: [%s][%s]', fileName, sheetName)
+    wb = xlwt.Workbook()
+    sh = wb.add_sheet(sheetName)
+
+    try:
+        logging.debug('write2excl')
+
+        #header
+        colId = 0
+        rowId = 0
+        for hCol in data.columns:
+            sh.write(rowId, colId, hCol)
+            colId += 1
+
+        rowId += 1
+        for vRow in data.values:
+            colId = 0
+            for vCol in vRow:
+                sh.write(rowId, colId, vCol)
+                colId += 1
+            rowId += 1
+
+    except Exception as ex:
+        logging.error('write2excel: [%s]', ex)
+        raise Exception (ex)
+    finally:
+        wb.save(fileName)
+
 
 def main():
-    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                        datefmt='%Y-%m-%d:%H:%M:%S',
-                        level=logging.DEBUG)
-    try:
-        data = read_json(filename="../data/sample-nest.json")
-        outFilename = "../output/data-nest.xlsx"
-        df = pd.json_normalize(data, max_level=2)
-        df.to_excel(outFilename)
-    except Exception as ex:
-        logging.error(ex)
-    finally:
-        logging.info('Done')
-
-
-def main_list():
     try:
 
-        srcFile = 'sample-nest-list-multi.json'
+        srcFile = 'sample-nest-list-multi-with-null.json'
         srcFullName = '../data/' + srcFile
         outFilePath = '../output/'
         outFileName = srcFile + '.xls'
@@ -44,12 +60,13 @@ def main_list():
         # df = pd.json_normalize(data)
 
         df = pd.json_normalize(data,
-                               meta=['id', 'firstName', 'lastName', 'country', ['contacts', 'email'], ['contacts', 'Mob']]
-                               , max_level=1, sep='->'
+                               meta=['id', 'firstName', 'lastName', 'country', ['contacts', 'email'],
+                                     ['contacts', 'Mob']]
+                               , max_level=1, sep='.'
                                , record_path=['funds'])
 
-
-        df.to_excel(outFullName)
+        # df.to_excel(outFullName)
+        write2excel(outFullName, 'S1', df)
     except Exception as ex:
         logging.error(ex)
     finally:
@@ -57,4 +74,7 @@ def main_list():
 
 
 if __name__ == '__main__':
-    main_list()
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.DEBUG)
+    main()
