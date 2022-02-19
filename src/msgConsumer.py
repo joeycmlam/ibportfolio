ba325e152472fae7885fb346562af15f81de19dc
joeylam <joey.cm.lam@gmail.com>
2022-02-20 00:49:32 +0800

    bug fix

--- src/msgConsumer.py
+++ src/msgConsumer.py
@@ -2,22 +2,24 @@ import pika
 import logging
 
 
+p_host = 'myhost01'
+p_queue = 'q_order'
+
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=p_host))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue=p_queue)
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        logging.info(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=p_queue, on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
     logging.info(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)s()] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
--- src/msgProducer.py
+++ src/msgProducer.py
@@ -1,30 +1,34 @@
-import logging
 import pika
+import logging
+
+p_host = 'myhost01'
+p_queue = 'q_order'
 
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(p_host))
 
     try:
         channel = connection.channel()
-        channel.queue_declare(queue='order')
+        channel.queue_declare(queue=p_queue)
 
         channel.basic_publish(exchange='',
-                              routing_key='order',
+                              routing_key=p_queue,
                               body='Hello World!')
     except Exception as err:
         raise err
     finally:
         connection.close()
 
-if __name__ == 'main':
+if __name__ == '__main__':
+
     logging.basicConfig(
         format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)s()] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
         level=logging.INFO)
     try:
         logging.info('start')
-        main()
+        # main()
         logging.info('completed')
     except Exception as err:
         logging.error(err)
